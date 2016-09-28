11b2af3b2bc1c88fc5bc78283c87beecd23b65c1
yogoken <yogo.k@husky.neu.edu>
2016-09-28 22:24:22 +0900

    Update article_uploader

--- app/uploaders/article_uploader.rb
+++ app/uploaders/article_uploader.rb
@@ -5,10 +5,16 @@ class ArticleUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  # include RMagi
+  Include CarrierWave::RMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  case Rails.env
+  when 'development'
+    storage :file
+  when 'production'
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -30,22 +36,25 @@ class ArticleUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
-
+  process resize_to_limit: [900, 600]
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process :resize_to_fit => [50, 50]
+  process :resize_to_fit => [50, 50]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
 
+  def filename
+    time = Time.zone.now
+    rand = SecureRandom.hex(2)
+    name = time.strftime('%Y%m%d%H%M%S') + '-' + rand + '.jpg'
+    name.downcase
+  end
 end
