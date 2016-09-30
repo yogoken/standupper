6175332377315d388f564d50c81ea9571315d4f3
yogoken <yogo.k@husky.neu.edu>
2016-09-30 15:43:26 +0900

    Create article#file spec as uniq

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,5 +1,9 @@
+include ActionDispatch::TestProcess
+
 FactoryGirl.define do
   factory :article do
-    
+    file    { fixture_file_upload Rails.root.join('public', 'sample.gif'), 'image/article' }
+    title   Faker::Name.title
+    content Faker::Lorem.paragraph
   end
 end
