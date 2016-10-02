fb881ba948fa499ea8d749e2cdfd57b22d06193a
yogoken <yogo.k@husky.neu.edu>
2016-10-02 19:01:52 +0900

    Includes geinin for articles

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def index
-    @articles = Article.newly.page params[:page]
+    @articles = Article.newly.page params[:page].includes(:geinin)
     @newly_articles = Article.newly.limit(3)
     @geinins = Geinin.bigger
   end
